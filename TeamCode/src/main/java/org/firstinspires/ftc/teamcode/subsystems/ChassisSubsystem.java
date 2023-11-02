d0685c6b24d2098a8d3336bf7f041aba78c545c3
comprefg <rfgarcia2023@gmail.com>
2023-11-01 18:34:28 -0600

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ChassisSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ChassisSubsystem.java
@@ -146,10 +146,10 @@ public class ChassisSubsystem {
     // Creating my odometry object from the kinematics object. Here,
 // our starting pose is 5 meters along the long end of the field and in the
 // center of the field along the short end, facing forward.
-    /*MecanumDriveOdometry m_odometry = new MecanumDriveOdometry
+    MecanumDriveOdometry m_odometry = new MecanumDriveOdometry
             (
-                    m_kinematics, mak,
+                    m_kinematics, new Rotation2d(0,0),
                     new Pose2d(5.0, 13.5, new Rotation2d())
-    );*/
+    );
 
 }
