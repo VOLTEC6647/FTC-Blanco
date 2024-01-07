2e756c3d6c640da597e10341f89e595a2c158811
rickyegl <a01287576@tec.mx>
2024-01-06 18:42:22 -0600

    pre vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
@@ -4,13 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.subsystems.CameraSubsystem;
+
 @Autonomous
 public class autonomous extends LinearOpMode {
+    private CameraSubsystem cameraSubsystem;
     @Override
     public void runOpMode() {
-        waitForStart();
-        while (opModeIsActive()){
+        cameraSubsystem = CameraSubsystem.getInstance(hardwareMap);
 
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
         }
     }
 }
